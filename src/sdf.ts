857bed7d9755412ceb9e57885afe8b5de55cefea
Joe Howarth <josephehowarth@gmail.com>
2019-06-30 15:17:58 -0400

    transparent text background

--- src/sdf.ts
+++ src/sdf.ts
@@ -1,5 +1,6 @@
-import {Engine, Mesh, RawTexture, Scene, ShaderMaterial, Texture, Vector4, VertexData} from "@babylonjs/core"
-import fontInfo from "./sdfFont"
+import {Engine, Mesh, RawTexture, Scene, ShaderMaterial, Texture, VertexData} from "@babylonjs/core"
+import font from "./sdfFont"
+import {backgroundVertexDeclaration} from "@babylonjs/core/Shaders/ShadersInclude/backgroundVertexDeclaration"
 
 const fontTextureSrc = "font_sdf.png"
 export function drawString(text: string, scene: Scene): Mesh {
@@ -7,6 +8,7 @@ export function drawString(text: string, scene: Scene): Mesh {
   let mesh = new Mesh("sdf_" + text, scene)
   mesh.material = material
   let vertexData = makeVerticesForString(text)
+  // let vertexData = getVertexData(text)
   vertexData.applyToMesh(mesh)
   console.log(vertexData)
   console.log(mesh)
@@ -19,8 +21,6 @@ export function sdfMaterial(scene: Scene): ShaderMaterial {
   const sdfTexture = new Texture(fontTextureSrc, scene, false, false)
   sdfTexture.onLoadObservable.add(() => {
     material.setTexture("u_texture", sdfTexture)
-    // material.setFloat("u_buffer", 8)
-    // material.setFloat("u_gamma", 3)
     console.log(material)
   })
   return material
@@ -40,21 +40,19 @@ function makeVerticesForString(s: string) {
   const texcoords = new Float32Array(numVertices * 2)
   let offset = 0
   let x = 0
-  const maxX = fontInfo.width
-  const maxY = fontInfo.height
-  const letterHeight = fontInfo.characters["a"].height
-  const letterWidth = fontInfo.characters["a"].width
+  const letterHeight = font.characters["a"].height
+  const letterWidth = font.characters["a"].width
   for (let ii = 0; ii < len; ++ii) {
     const letter = s[ii]
     // @ts-ignore
-    const glyphInfo = <CharacterInfo>fontInfo.characters[letter]
+    const glyphInfo = <CharacterInfo>font.characters[letter]
     console.log(glyphInfo)
     if (glyphInfo) {
       const x2 = x + glyphInfo.width
-      const u1 = glyphInfo.x / maxX
-      const v1 = (glyphInfo.y + glyphInfo.height - 1) / maxY
-      const u2 = (glyphInfo.x + glyphInfo.width - 1) / maxX
-      const v2 = glyphInfo.y / maxY
+      const u1 = glyphInfo.x / font.width
+      const v1 = (glyphInfo.y + glyphInfo.height) / font.height
+      const u2 = (glyphInfo.x + glyphInfo.width) / font.width
+      const v2 = glyphInfo.y / font.height
       console.log(x, x2, glyphInfo.height)
       console.log(u1,v1, u2, v2)
       
@@ -89,11 +87,11 @@ function makeVerticesForString(s: string) {
       texcoords[offset + 10] = u2;
       texcoords[offset + 11] = v2;
       
-      x += glyphInfo.width + glyphInfo.advance;
+      x += glyphInfo.advance;
       offset += 12;
     } else {
       // we don't have this character so just advance
-      x += fontInfo.characters[' '].width;
+      x += font.characters[' '].width;
     }
   }
   
@@ -101,8 +99,8 @@ function makeVerticesForString(s: string) {
   const pos = new Float32Array(numVertices * 3)
   const indices = new Uint16Array(numVertices)
   for (let i = 0; i < numVertices; i++) {
-    pos[i*3] = positions[i*2]
-    pos[i*3+1] = positions[i*2+1]
+    pos[i*3] = positions[i*2]      / font.characters['a'].width
+    pos[i*3+1] = positions[i*2+1]  / font.characters['a'].width
     pos[i*3+2] = 0
     indices[i] = i
   }
@@ -134,4 +132,114 @@ function initShader(scene: Scene): ShaderMaterial {
     // mat.setVector4("u_color", new Vector4(0.5,0.5,1,1))
   }
   return mat
+}
+
+
+function getVertexData(text: string): VertexData {
+  let totalAdvance = 0
+  // Measure how wide the text is
+  for (var i = 0; i < text.length; i++) {
+    // @ts-ignore
+    totalAdvance += font.characters[text[i]].advance;
+  }
+  
+  // Center the text at the origin
+  // let x = -totalAdvance / 2
+  let x = 0;
+  const y = font.size / 2
+  let verticesAndUvs = []
+  const numVertices = text.length * 6;
+  let vertices = new Float32Array( numVertices * 3)
+  let uvs = new Float32Array(numVertices * 2)
+  let indices = new Uint16Array(numVertices)
+  
+  // Add a quad for each character
+  for (let i = 0; i < text.length; i++) {
+    let c = <CharacterInfo>font.characters[text[i]];
+    const vert = i * 6
+    
+    // p0 --- p1
+    // | \     |
+    // |   \   |
+    // |     \ |
+    // p2 --- p3
+    
+    var x0 = x - c.originX;
+    var y0 = y - c.originY;
+    var s0 = c.x / font.width;
+    var t0 = c.y / font.height;
+    
+    var x1 = x - c.originX + c.width;
+    var y1 = y - c.originY;
+    var s1 = (c.x + c.width) / font.width;
+    var t1 = c.y / font.height;
+    
+    var x2 = x - c.originX;
+    var y2 = y - c.originY + c.height;
+    var s2 = c.x / font.width;
+    var t2 = (c.y + c.height) / font.height;
+    
+    var x3 = x - c.originX + c.width;
+    var y3 = y - c.originY + c.height;
+    var s3 = (c.x + c.width) / font.width;
+    var t3 = (c.y + c.height) / font.height;
+    let a = vert * 3
+    vertices[a] = x0
+    vertices[a + 1] = y0
+    vertices[a + 2] = 0
+    a += 3
+    vertices[a] = x1
+    vertices[a + 1] = y1
+    vertices[a + 2] = 0
+    a += 3
+    vertices[a] = x3
+    vertices[a + 1] = y3
+    vertices[a + 2] = 0
+    a += 3
+    vertices[a] = x0
+    vertices[a + 1] = y0
+    vertices[a + 2] = 0
+    a += 3
+    vertices[a] = x3
+    vertices[a + 1] = y3
+    vertices[a + 2] = 0
+    a += 3
+    vertices[a] = x2
+    vertices[a + 1] = y2
+    vertices[a + 2] = 0
+  
+    let b = vert * 2
+    uvs[b] = s0
+    uvs[b+1] = t0
+    b += 2
+    uvs[b] = s1
+    uvs[b+1] = t1
+    b += 2
+    uvs[b] = s3
+    uvs[b+1] = t3
+    b += 2
+    uvs[b] = s0
+    uvs[b+1] = t0
+    b += 2
+    uvs[b] = s3
+    uvs[b+1] = t3
+    b += 2
+    uvs[b] = s2
+    uvs[b+1] = t2
+    
+    // verticesAndUvs.push(x0, y0, s0, t0);
+    // verticesAndUvs.push(x1, y1, s1, t1);
+    // verticesAndUvs.push(x3, y3, s3, t3);
+    //
+    // verticesAndUvs.push(x0, y0, s0, t0);
+    // verticesAndUvs.push(x3, y3, s3, t3);
+    // verticesAndUvs.push(x2, y2, s2, t2);
+    
+    x += c.advance;
+  }
+  let data = new VertexData()
+  data.positions = vertices
+  data.uvs = uvs
+  data.indices = indices
+  return data
 }
